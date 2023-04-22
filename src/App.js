7cf1ae308cc2c53f3065153fb30398a60cbfae8e
Amanda <amandabaratadev@gmail.com>
2023-04-22 22:36:37 +0100

    finish zip code project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi'
+import './style.css'
+import api from './Services/api';
+
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [zip, setZip] = useState({});
+
+  async function handleSearch() {
+    if (input === '') {
+      alert("please fullfill zip code")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setZip(response.data)
+      setInput('');
+
+    } catch {
+      alert("Error to catch zip code")
+      setInput('')
+
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1 className="title">Zip Code Finder</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Type your zip code:"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="FFF" />
+        </button>
+      </div>
+
+      {Object.keys(zip).length > 0 && (
+       <main className='main'>
+       <h2> Zip Code: {zip.cep}</h2>
+
+       <span>{zip.logradouro} </span>
+       <span>Complemento: {zip.complemento}</span>
+       <span>{zip.bairro}</span>
+       <span>{zip.localidade} - {zip.uf}</span>
+       
+     </main>
+      )}
+     
+
+     
     </div>
   );
 }
